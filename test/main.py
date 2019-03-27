330d1b97c59e60e9a89ab75139d70603b510d795
Wei Jie <ngweijie94@gmail.com>
2019-03-27 16:52:31 +0800

    initial commit

---
+++ test/main.py
@@ -0,0 +1,17 @@
+# https://www.tensorflow.org/tutorials/keras/basic_classification
+# https://www.tensorflow.org/tutorials/estimators/cnn
+from __future__ import absolute_import, division, print_function
+
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+# print(tf.__version__)
+
